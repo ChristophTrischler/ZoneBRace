68c152dc9642e46da39882fe5855280f1c3f2486
CHr <christoph.trischler@t-online.de>
2024-03-03 03:12:16 +0100

    new style

--- backend/build.rs
+++ backend/build.rs
@@ -3,8 +3,10 @@ use std::process::Command;
 fn main() {
     println!("cargo:rerun-if-changed=templates/");
     //println!("cargo:rerun-if-changed=tailwind.config.js");
-    Command::new("tailwindcss")
+    if let Err(e) = Command::new("tailwindcss")
         .args(&["-o", "static/style.css"])
         .status()
-        .ok();
+    {
+        eprintln!("{e:?}");
+    }
 }
